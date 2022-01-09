d4f0f71491fdb2c98d63485179604d0207147245
Mateus Kienzle <mateuskienzle@gmail.com>
2022-01-09 20:20:48 -0300

    Add more five files with basic concepts of JS

---
+++ 4_string/scripts.js
@@ -0,0 +1,11 @@
+console.log("Oi, meu nome é Mateus");
+console.log('Oi, meu nome é Mateus');
+console.log(`Oi, meu nome é Mateus`);
+console.log("Infinity");
+console.log(Infinity);
+
+console.log(typeof "Oi, meu nome é Mateus");
+console.log(typeof 'Oi, meu nome é Mateus');
+console.log(typeof `Oi, meu nome é Mateus`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
---
+++ 5_string_avancado/scripts.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \nSegunda linha");
+console.log("O meu nome é 'Mateus'");
+console.log('O meu nome é "Mateus"');
+console.log(`A multiplicação de 5 por 3 é ${5*3}`);
+console.log("O" + " meu" + " nome" + " é" + " Mateus");
