a077d96c23381a8b351fa967827bf6216b6771e8
Mateus Kienzle <mateuskienzle@gmail.com>
2022-01-18 01:32:26 -0300

    Add the 'continue' and 'switch' concepts

---
+++ secao_4/19_switch/scripts.js
@@ -0,0 +1,14 @@
+let nome = "Mateus";
+
+switch(nome){
+    
+    case "Mateus":
+        console.log("O nome é Mateus");
+        break;
+    case "João":
+        console.log("O nome é João");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+        break;
+}
