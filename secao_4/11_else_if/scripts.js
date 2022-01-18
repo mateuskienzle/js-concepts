19bebbb85cf2b39f60c708bdc4d8eb9285beaa31
Mateus Kienzle <mateuskienzle@gmail.com>
2022-01-17 23:48:54 -0300

    Add more basic concepts

---
+++ secao_4/11_else_if/scripts.js
@@ -0,0 +1,12 @@
+let nome = "Mateus";
+let idade = 28;
+
+if(nome != undefined && nome =="Joaquim"){
+    console.log("Nome está definido");
+}
+else if (nome == "Mateus" && nome.length >5 && idade == 50) {
+    console.log("O nome é Mateus");
+}
+else{
+    console.log("Não é Mateus");
+}
