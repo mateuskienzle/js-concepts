fca7340c15f8231520b83a93e0713a417a8b239d
Mateus Kienzle <mateuskienzle@gmail.com>
2022-01-25 23:38:38 -0300

    Add exercise with if/else if concepts

---
+++ secao_5/6_exercicio_11/script.js
@@ -0,0 +1,20 @@
+let idade = 19;
+let cnh = true;
+
+if(idade >= 18 && cnh == false){
+
+    console.log("Tem idade permitida, mas não tem CNH. Não pode drigir!");
+
+    }
+
+else if (idade >= 18 && cnh == true){
+
+    console.log("Pode dirigir!");
+    
+}
+
+else {
+
+    console.log("Não pode dirigir!");
+    
+}
